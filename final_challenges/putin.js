69ac6b4a208abdf6057a7e0fee3eee59ce3233bd
gretchenziegler <gretchenziegler@gmail.com>
2017-08-13 14:40:25 -0400

    Create final challenges

---
+++ final_challenges/putin.js
@@ -0,0 +1,13 @@
+putinsBrain = {
+  launchCode: "a5Mjp257GHMGH23e5qxE",
+  bffs: [],
+  tortureCount: 931
+}
+
+// Print the launch code to the console.
+
+// Add "Donald Trump" as one of Putin's BFFs.
+
+// Add the key-value pair favoriteHobby: "Riding ponies".
+
+// It's been a productive day. Update the torture count to 987.
