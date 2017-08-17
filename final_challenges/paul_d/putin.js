ceb5b04cea494372a771158ee5e012ecb69635fa
${fullname} <${vitalsemail}>
2017-08-17 13:35:08 -0400

    Newer one

--- final_challenges/paul_d/putin.js
+++ final_challenges/paul_d/putin.js
@@ -7,9 +7,11 @@ putinsBrain = {
 // Below each comment below, write code that executes the directions in the comment, using the data above.
 
 // Print the launch code to the console.
-
+console.log(putinsBrain.launchCode);
 // Add "Donald Trump" as one of Putin's BFFs.
-
+putinsBrain.bffs.push("Donald Trump");
 // Add the key-value pair favoriteHobby: "Riding ponies".
-
+putinsBrain.favoriteHobby = "Riding ponies";
 // It's been a productive day. Update the torture count to 987.
+putinsBrain.tortureCount = 987;
+console.log(putinsBrain);
